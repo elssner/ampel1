ee0d5408788637046153358fd8bc5ae1b5ab381e
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-10-14 19:46:20 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,23 @@
 pins.onPulsed(DigitalPin.P1, PulseValue.Low, function () {
     Int += 1
-    lcd16x2rgb.writeText(lcd16x2rgb.eADDR_LCD.LCD_16x2, 1, 0, 15, lcd16x2rgb.eAlign.left, "Int " + Int)
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 0, 15, "Int " + Int)
 })
 input.onButtonEvent(Button.A, ButtonEvent.Hold, function () {
     Int = 0
 })
 let Int = 0
-lcd16x2rgb.initLCD(lcd16x2rgb.eADDR_LCD.LCD_16x2)
+lcd16x2rgb.initLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2))
 qwiicgpio.setMode(
-qwiicgpio.eADDR.GPIO_Qwiic,
+qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x27),
 qwiicgpio.eIO.IN,
 qwiicgpio.eIO.IN_inverted,
-qwiicgpio.eIO.OUT,
-qwiicgpio.eIO.OUT,
-qwiicgpio.eIO.OUT,
-qwiicgpio.eIO.OUT,
-qwiicgpio.eIO.OUT,
-qwiicgpio.eIO.OUT
+qwiicgpio.eIO.IN,
+qwiicgpio.eIO.IN,
+qwiicgpio.eIO.IN,
+qwiicgpio.eIO.IN,
+qwiicgpio.eIO.IN,
+qwiicgpio.eIO.IN
 )
 loops.everyInterval(500, function () {
-    lcd16x2rgb.writeText(lcd16x2rgb.eADDR_LCD.LCD_16x2, 0, 0, 15, lcd16x2rgb.eAlign.left, bit.formatNumber(qwiicgpio.readINPUT_PORT(qwiicgpio.eADDR.GPIO_Qwiic), bit.eLength.BIN_11111111))
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 0, 0, 15, bit.formatNumber(qwiicgpio.readINPUT_PORT(qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x27)), bit.eLength.BIN_11111111))
 })
